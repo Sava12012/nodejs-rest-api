5cba51f3880123eaa60c63d642b61a19dea2eb9a
Dmytro Savych <sava.savych@gmail.com>
2023-03-15 19:48:29 +0200

    add midlware - Validid and Mongoose - Shema

---
+++ middlewares/isValidid.js
@@ -0,0 +1,13 @@
+const { isValidObjectId } = require("mongoose");
+
+const { linkError } = require("../../helpers/linkError");
+
+const isValidId = (req, res, next) => {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    next(linkError(404, "Invalid id"));
+  }
+  next();
+};
+
+module.exports = isValidId;
--- models/contacts.js
+++ models/contacts.js
@@ -1,63 +1,22 @@
-const path = require("path");
-const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
+const { Schema, model } = require("mongoose");
 
-const contactsPath = path.resolve("models", "contacts.json");
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
-};
-const getContactById = async (contactId) => {
-  const contactsArr = await listContacts(contactsPath);
-  const oneContact = contactsArr.find((item) => item.id === contactId);
-  return oneContact || null;
-};
-const addContact = async ({ name, email, phone }) => {
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  const contactsArr = await listContacts(contactsPath);
-  contactsArr.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contactsArr, null, 2));
-  return newContact;
-};
+const Contact = model("contact", contactSchema);
 
-const removeContact = async (contactId) => {
-  const contactsArr = await listContacts(contactsPath);
-  const indexRemoveContact = contactsArr.findIndex(
-    (item) => item.id === contactId
-  );
-  if (indexRemoveContact === -1) {
-    return null;
-  }
-  const [result] = contactsArr.splice(indexRemoveContact, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contactsArr, null, 2));
-  return result;
-};
-const updateContact = async (contactId, body) => {
-  const contactsArr = await listContacts(contactsPath);
-  const indexUpdateContact = contactsArr.findIndex(
-    (item) => item.id === contactId
-  );
-  if (indexUpdateContact === -1) {
-    return null;
-  }
-  contactsArr[indexUpdateContact] = {
-    contactId,
-    ...body,
-  };
-  await fs.writeFile(contactsPath, JSON.stringify(contactsArr, null, 2));
-  return contactsArr[indexUpdateContact];
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = Contact;
