f8757b4ddcaf6c7d8f8b0af323e6d960919420b5
Dmytro Savych <sava.savych@gmail.com>
2023-03-28 18:51:35 +0300

    create users avatars

--- models/user.js
+++ models/user.js
@@ -1,28 +1,29 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-        password: {
-          type: String,
-          required: [true, 'Set password for user'],
-        },
-        email: {
-          type: String,
-          required: [true, 'Email is required'],
-          unique: true,
-        },
-        subscription: {
-          type: String,
-          enum: ["starter", "pro", "business"],
-          default: "starter"
-        },
-        token: String
-})
+  password: {
+    type: String,
+    required: [true, "Set password for user"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  avatarUrl: String,
+  token: String,
+});
 
 userSchema.post("save", (err, data, next) => {
-  const {name, code} = err
-  err.status = (name === "MongoServerError" && code === 11000) ? 409 : 400
-  next()
-})
-const User = model("user", userSchema)
+  const { name, code } = err;
+  err.status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  next();
+});
+const User = model("user", userSchema);
 
-module.exports = User
+module.exports = User;
