49352be525e4afc42a4b1d55d942f8d1822cedc3
Dmytro Savych <sava.savych@gmail.com>
2023-04-07 17:30:39 +0300

    add verifyEmail

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -1,15 +1,19 @@
-const getContacts = require('./getContacts')
-const getContactById = require('./getContactById')
-const postContact = require('./postContact')
-const deleteContact = require('./deleteContact')
-const updateContact = require('./updateContact')
-const updateFavoruteContact = require('./updateFavoriteContact')
+const getContacts = require("./getContacts");
+const getContactById = require("./getContactById");
+const postContact = require("./postContact");
+const deleteContact = require("./deleteContact");
+const updateContact = require("./updateContact");
+const updateFavoruteContact = require("./updateFavoriteContact");
+const verifyEmail = require("./verifyEmail");
+const resendVerify = require("./resendVerify");
 
 module.exports = {
-    getContacts,
-    getContactById,
-    postContact,
-    deleteContact,
-    updateContact,
-    updateFavoruteContact
-}
+  getContacts,
+  getContactById,
+  postContact,
+  deleteContact,
+  updateContact,
+  updateFavoruteContact,
+  verifyEmail,
+  resendVerify,
+};
